5ef21995133e8f93f80ee3a05d34202803c9e2e9
Avnullah Ates <avnullahates@gmail.com>
2022-09-20 22:30:43 +0300

    first structure created

---
+++ EntityLayer/Concrete/Portfolio.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace EntityLayer.Concrete
+{
+    public class Portfolio
+    {
+        [Key]
+        public int PortfolioID { get; set; }
+
+        public string Name { get; set; }
+
+        public string ImageURL { get; set; }
+    }
+}
