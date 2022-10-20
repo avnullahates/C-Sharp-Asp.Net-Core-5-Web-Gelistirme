05cb1c5b9b7ea65a975b875c64bfd4ddbaac8d31
Avnullah Ates <avnullahates@gmail.com>
2022-10-20 13:49:50 +0300

    added writer panel

--- CoreProjeMVC/Startup.cs
+++ CoreProjeMVC/Startup.cs
@@ -52,6 +52,13 @@ namespace CoreProjeMVC
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                  name: "areas",
+                  pattern: "{area:exists}/{controller=Default}/{action=Index}/{id?}"
+                );
+            });
         }
     }
 }
